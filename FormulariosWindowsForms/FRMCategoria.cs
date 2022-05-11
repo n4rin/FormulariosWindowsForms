5b2b2b72bac2719227b93593e55184fbe886be9c
vxpresto <nallinguilherme@gmail.com>
2022-05-11 17:29:08 -0300

    add versao3

--- FormulariosWindowsForms/FRMCategoria.cs
+++ FormulariosWindowsForms/FRMCategoria.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace FormulariosWindowsForms
 {
     public partial class FRMCategoria : Form
     {
+        private Categoria categoria = new Categoria();
+        private List<Categoria> lstCategoria = new List<Categoria>();
+        private BindingSource bsCategoria;
+
+
         private bool Insercao = false;
         private bool Edicao = false;
         private FRMCategoria fCategoria;
@@ -19,6 +25,7 @@ namespace FormulariosWindowsForms
         public FRMCategoria()
         {
             InitializeComponent();
+            lstCategoria = categoria.GeraCategorias();
         }
 
         public void LimparCampos ()
@@ -48,10 +55,43 @@ namespace FormulariosWindowsForms
 
         private void FRMCategoria_Load(object sender, EventArgs e)
         {
+            dgCategoria.ColumnCount = 5;
+            dgCategoria.AutoGenerateColumns = false;
+            dgCategoria.Columns[0].Width = 50;
+            dgCategoria.Columns[0].HeaderText = "ID";
+            dgCategoria.Columns[0].DataPropertyName = "Id";
+            dgCategoria.Columns[0].Visible = false;
+            dgCategoria.Columns[1].Width = 200;
+            dgCategoria.Columns[1].HeaderText = "NOME";
+            dgCategoria.Columns[1].DataPropertyName = "Nome";
+            dgCategoria.Columns[2].Width = 400;
+            dgCategoria.Columns[2].HeaderText = "DESCRIÇÃO";
+            dgCategoria.Columns[2].DataPropertyName = "Descricao";
+            dgCategoria.Columns[3].Width = 50;
+            dgCategoria.Columns[3].HeaderText = "TIPO";
+            dgCategoria.Columns[3].DataPropertyName = "Tipo";
+            dgCategoria.Columns[4].Width = 50;
+            dgCategoria.Columns[4].HeaderText = "STATUS";
+            dgCategoria.Columns[4].DataPropertyName = "Status";
+
+            dgCategoria.AllowUserToAddRows = false;
+            dgCategoria.AllowUserToDeleteRows = false;
+            dgCategoria.MultiSelect = false;
+            dgCategoria.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+
             txtNome.Text = "Combustivel";
             txtDescricao.Text = "Consumo de combustivel";
             rdDespesa.Checked = true;
             chkStatus.Checked = true;
+        }
+
+        public void carregaGridCategoria()
+        {
+            bsCategoria = new BindingSource();
+            bsCategoria.DataSource = lstCategoria;
+            dgCategoria.DataSource = bsCategoria;
+            dgCategoria.Refresh();
         }
 
         private void novoRegistro(object sender, EventArgs e)
@@ -82,7 +122,7 @@ namespace FormulariosWindowsForms
                 fCategoria.Activate();
             }
 
-            fCategoria.MdiParent = this;
+            //fCategoria.MdiParent = this;
             fCategoria.Show();
         }
 
